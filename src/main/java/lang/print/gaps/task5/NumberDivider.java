6c3acc8fdcff9bc29e04fff05dc67a89288e34bc
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-06-30 12:04:56 +0500

    Third commit

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        double result = toBeDivided / 5;
+        float convertToFloat = toBeDivided;
+        float result = convertToFloat / 5;
         System.out.println(result);
     }
 }
